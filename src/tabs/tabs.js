1bf57b5ec0832460e480d334a95a51773462279d
Roman Heinrich <roman.heinrich@gmail.com>
2013-07-27 23:30:28 +0200

    Squashed 'github.com/angular-ui/bootstrap/' changes from d9e5bc9..ba1f741
    ba1f741 fix(tabs): if tab is active at start, always select it
    a51c309 docs(typeahead): clarify subset of select's syntax used
    220e7b6 feat(tabs): add the ability to set the direction of the tabs
    
    git-subtree-dir: github.com/angular-ui/bootstrap
    git-subtree-split: ba1f741dff4b6eb3c624f7020e663e562129cc79

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -30,7 +30,7 @@ function TabsetCtrl($scope, $element) {
 
   ctrl.addTab = function addTab(tab) {
     tabs.push(tab);
-    if (tabs.length == 1 || tab.active) {
+    if (tabs.length === 1 || tab.active) {
       ctrl.select(tab);
     }
   };
@@ -56,6 +56,8 @@ function TabsetCtrl($scope, $element) {
  * Tabset is the outer container for the tabs directive
  *
  * @param {boolean=} vertical Whether or not to use vertical styling for the tabs.
+ * @param {string=} direction  What direction the tabs should be rendered. Available:
+ * 'right', 'left', 'below'.
  *
  * @example
 <example module="ui.bootstrap">
@@ -77,12 +79,19 @@ function TabsetCtrl($scope, $element) {
     restrict: 'EA',
     transclude: true,
     replace: true,
+    require: '^tabset',
     scope: {},
     controller: 'TabsetController',
     templateUrl: 'template/tabs/tabset.html',
-    link: function(scope, element, attrs) {
-      scope.vertical = angular.isDefined(attrs.vertical) ? scope.$eval(attrs.vertical) : false;
-      scope.type = angular.isDefined(attrs.type) ? scope.$parent.$eval(attrs.type) : 'tabs';
+    compile: function(elm, attrs, transclude) {
+      return function(scope, element, attrs, tabsetCtrl) {
+        scope.vertical = angular.isDefined(attrs.vertical) ? scope.$eval(attrs.vertical) : false;
+        scope.type = angular.isDefined(attrs.type) ? scope.$parent.$eval(attrs.type) : 'tabs';
+        scope.direction = angular.isDefined(attrs.direction) ? scope.$parent.$eval(attrs.direction) : 'top';
+        scope.tabsAbove = (scope.direction != 'below');
+        tabsetCtrl.$scope = scope;
+        tabsetCtrl.$transcludeFn = transclude;
+      };
     }
   };
 })
@@ -193,6 +202,7 @@ function($parse, $http, $templateCache, $compile) {
           scope.$parent.$watch(getActive, function updateActive(value) {
             scope.active = !!value;
           });
+          scope.active = getActive(scope.$parent);
         } else {
           setActive = getActive = angular.noop;
         }
@@ -202,8 +212,7 @@ function($parse, $http, $templateCache, $compile) {
           if (active) {
             tabsetCtrl.select(scope);
             scope.onSelect();
-          }
-          else {
+          } else {
             scope.onDeselect();
           }
         });
@@ -283,6 +292,25 @@ function($parse, $http, $templateCache, $compile) {
     );
   }
 }])
+
+.directive('tabsetTitles', function($http) {
+  return {
+    restrict: 'A',
+    require: '^tabset',
+    templateUrl: 'template/tabs/tabset-titles.html',
+    replace: true,
+    link: function(scope, elm, attrs, tabsetCtrl) {
+      if (!scope.$eval(attrs.tabsetTitles)) {
+        elm.remove();
+      } else {
+        //now that tabs location has been decided, transclude the tab titles in
+        tabsetCtrl.$transcludeFn(tabsetCtrl.$scope.$parent, function(node) {
+          elm.append(node);
+        });
+      }
+    }
+  };
+})
 
 ;
 
