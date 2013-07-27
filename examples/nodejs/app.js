085f922640c2a87c4101297c632be2ca3aa9e15d
Roman Heinrich <roman.heinrich@gmail.com>
2013-07-27 13:09:49 +0200

    Squashed 'github.com/twilson63/ngUpload/' changes from a655b47..c0bbced
    c0bbced Merge pull request #55 from hybrisCole/master
    fa600f9 Changing from express ~3.1.0 to 3.2.0
    770bba2 Update readme.md
    3c14938 Release v0.3.11
    515e088 Merge pull request #53 from whyjustin/master
    e4d1a8b Check if in a $digest cycle before calling functions through $apply
    35037a7 version 0.3.10
    01f8ef7 Release v0.3.10
    02f1101 Merge branch 'master' of github.com:twilson63/ngUpload
    77f36df updated version
    70cff0d Merge pull request #51 from kelp404/master
    4a494ea updated git ignore
    0be9ff1 removed .DS_Store
    b6bcd5d upgraded to support crsf #50
    4ae30a5 Merge pull request #50 from mguymon/master
    07d4d8e add area for directive options
    21587a2 add upload-options-convert-hidden to set the value of ng-model as the hidden input value
    55dff6b remove debug logging
    2acfd88 assert that the ng-model is populating the input for submit
    61042ad add option to enable rails csrf, copy ng-model values to input values on submit
    4e21a35 Update readme.md
    9cde261 minified...
    ab31a5a Fixed #43 - if able to parse as json then resp is return to callback as json, otherwise, just text
    3e59846 Update readme.md
    bda29ee #34 changed to bind to iframe load after iframe is added to dom per @denyo
    1bd94bd Update readme.md
    
    git-subtree-dir: github.com/twilson63/ngUpload
    git-subtree-split: c0bbced1bae35702a09d1ac7cebf7e970c56ec70

--- examples/nodejs/app.js
+++ examples/nodejs/app.js
@@ -11,14 +11,15 @@ app.post('/upload', function (req, res) {
         function () {
             res.setHeader('Content-Type', 'text/html');
             if (req.files.length == 0 || req.files.file.size == 0)
-                res.send('No file uploaded at ' + new Date().toString());
+                res.send({ msg: 'No file uploaded at ' + new Date().toString() });
             else {
                 var file = req.files.file;
                 fs.unlink(file.path, function (err) {
                     if (err)
                         throw err;
                     else
-                        res.send('<b>"' + file.name + '"<b> uploaded to the server at ' + new Date().toString());
+                        res.end("Hello");
+                        //res.send({ msg: '<b>"' + file.name + '"</b> uploaded to the server at ' + new Date().toString() });
                 });
             }
         },
