1e1600093a537070233bc0dd2ca1851aef2e711a
Roman Heinrich <roman.heinrich@gmail.com>
2013-07-27 13:07:33 +0200

    Squashed 'github.com/angular-app/angular-app/' changes from 479b528..9e3d42e
    9e3d42e docs(README): fix code layout
    ec8d1f5 docs(README): clarify in which folder to run grunt
    5df7bef style(crud): fix spelling error
    c227570 Merge branch 'master' of git://github.com/cironunes/angular-app into cironunes-master
    9b4d5cd fix(assets): fix reference to bootstrap icons
    5cfe45a fix(admin-users): add dependency on the security.authorization module
    894872e chore(initDB): display better error message
    330c082 docs(README): move build after config
    4f721e6 fix(projects) now only authenticated people can see projects
    fe40d33 fix(projects-edit): fix typo
    40785cb fix(admin-projects-edit): ensure selTeamMember is in correct scope
    fd7d1b2 chore(less-watch): fix and improve less watch
    877c04f Merge pull request #159 from angular-app/remove_e2e
    ba7c1d6 chore(e2e): remove e2e tests with the scenario-runner
    8c2bb92 fix(README): update after migration to Grunt 0.4
    2fe3e51 fix(LoginController): clearForm should change `$scope.user` not `user`
    
    git-subtree-dir: github.com/angular-app/angular-app
    git-subtree-split: 9e3d42e235c8c8b5335c215e1bc31bccab5ef363

--- client/gruntFile.js
+++ client/gruntFile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
   // Default task.
   grunt.registerTask('default', ['jshint','build','karma:unit']);
   grunt.registerTask('build', ['clean','html2js','concat','recess:build','copy:assets']);
-  grunt.registerTask('release', ['clean','html2js','uglify','jshint','karma:unit','concat:index', 'recess:min','copy:assets','karma:e2e']);
+  grunt.registerTask('release', ['clean','html2js','uglify','jshint','karma:unit','concat:index', 'recess:min','copy:assets']);
   grunt.registerTask('test-watch', ['karma:watch']);
 
   // Print a timestamp (useful for when watching)
@@ -45,7 +45,8 @@ module.exports = function (grunt) {
         app: ['src/app/**/*.tpl.html'],
         common: ['src/common/**/*.tpl.html']
       },
-      less: ['src/less/stylesheet.less'] // recess:build doesn't accept ** in its file patterns
+      less: ['src/less/stylesheet.less'], // recess:build doesn't accept ** in its file patterns
+      lessWatch: ['src/less/**/*.less']
     },
     clean: ['<%= distdir %>/*'],
     copy: {
@@ -55,7 +56,6 @@ module.exports = function (grunt) {
     },
     karma: {
       unit: { options: karmaConfig('test/config/unit.js') },
-      e2e: { options: karmaConfig('test/config/e2e.js') },
       watch: { options: karmaConfig('test/config/unit.js', { singleRun:false, autoWatch: true}) }
     },
     html2js: {
@@ -153,11 +153,11 @@ module.exports = function (grunt) {
     },
     watch:{
       all: {
-        files:['<%= src.js %>', '<%= src.specs %>', '<%= src.less =>', '<%= src.tpl.app %>', '<%= src.tpl.common %>', '<%= src.html %>'],
+        files:['<%= src.js %>', '<%= src.specs %>', '<%= src.lessWatch %>', '<%= src.tpl.app %>', '<%= src.tpl.common %>', '<%= src.html %>'],
         tasks:['default','timestamp']
       },
       build: {
-        files:['<%= src.js %>', '<%= src.specs %>', '<%= src.less =>', '<%= src.tpl.app %>', '<%= src.tpl.common %>', '<%= src.html %>'],
+        files:['<%= src.js %>', '<%= src.specs %>', '<%= src.lessWatch %>', '<%= src.tpl.app %>', '<%= src.tpl.common %>', '<%= src.html %>'],
         tasks:['build','timestamp']
       }
     },
